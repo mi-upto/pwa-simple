883f4767633d67a284f8a842cd2b389987f1cbe0
mi <misato1989x@gmail.com>
2021-01-13 18:13:15 +0900

    add: service worker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
+import { Workbox } from 'workbox-window';
 import '../styles/index.scss';
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
+
+const wb = new Workbox('/sw.js');
+wb.register();
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{css,html,js,png,ico,svg,json}"
+  ],
+  "swDest": "build/sw.js"
+};
