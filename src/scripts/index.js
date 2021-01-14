f41e90a8f10aef9faa167c6be5d762eab81a7a8f
mi <misato1989x@gmail.com>
2021-01-14 17:45:50 +0900

    tweak: html
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # ♻️  :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # for http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,6 @@
 import { Workbox } from 'workbox-window';
 import '../styles/index.scss';
 
-if (process.env.NODE_ENV === 'development') {
-  require('../index.html');
-}
-
 if (process.env.NODE_ENV === 'production') {
   const wb = new Workbox('/sw.js');
   wb.register();
